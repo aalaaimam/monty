4159fdbe2f502e500b05c2b5e90f577d266dde6e
aalaaimam <aalaaimam.com>
2024-04-23 13:02:21 -0700

    monty

--- main.c
+++ main.c
@@ -8,14 +8,12 @@
 */
 int main(int argc, char *argv[])
 {
-stack_t *head = NULL; /* Declare head locally in main() */
-
 if (argc != 2)
 {
 fprintf(stderr, "USAGE: monty file\n");
 exit(EXIT_FAILURE);
 }
-open_file(argv[1]); /* Pass only the filename */
-free_nodes(); /* No need to pass anything */
+open_file(argv[1]);
+free_nodes();
 return (0);
 }
