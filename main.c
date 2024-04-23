590b2b6b85fdc8dc6310b7b7f16176367d91d12f
aalaaimam <aalaaimam.com>
2024-04-23 13:17:54 -0700

    monty

--- main.c
+++ main.c
@@ -1,19 +1,14 @@
 #include "monty.h"
 
-/**
-* main - entry point
-* @argc: arguments count
-* @argv: list of arguments
-* Return: always 0
-*/
-int main(int argc, char *argv[])
-{
-if (argc != 2)
-{
+int main(int argc, char *argv[]) {
+stack_t *head = NULL; /* Declare head locally in main() */
+
+if (argc != 2) {
 fprintf(stderr, "USAGE: monty file\n");
 exit(EXIT_FAILURE);
 }
-open_file(argv[1]);
-free_nodes();
-return (0);
+
+open_file(argv[1], &head); /* Pass head as a parameter */
+free_nodes(&head); /* Pass head as a parameter */
+return 0;
 }
