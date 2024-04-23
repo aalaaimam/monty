4f54c23accfbfa031fca20df16fe300ffca97255
aalaaimam <aalaaimam.com>
2024-04-23 12:10:26 -0700

    monty

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 #include "monty.h"
-stack_t *head = NULL;
 
 /**
 * main - entry point
@@ -9,72 +8,14 @@ stack_t *head = NULL;
 */
 int main(int argc, char *argv[])
 {
+stack_t *head = NULL; // Declare head locally in main()
+
 if (argc != 2)
 {
 fprintf(stderr, "USAGE: monty file\n");
 exit(EXIT_FAILURE);
 }
-open_file(argv[1]); /* Pass head as a parameter */
-free_nodes(); /* Pass head as a parameter */
+open_file(argv[1], &head); // Pass head as a parameter
+free_nodes(&head); // Pass head as a parameter
 return (0);
-}
-
-/**
-* create_node - Creates a node.
-* @n: Number to go inside the node.
-* Return: Upon success a pointer to the node. Otherwise NULL.
-*/
-stack_t *create_node(int n)
-{
-stack_t *node;
-
-node = malloc(sizeof(stack_t));
-if (node == NULL)
-err(4);
-node->next = NULL;
-node->prev = NULL;
-node->n = n;
-return (node);
-}
-
-/**
-* free_nodes - Frees nodes in the stack.
-*/
-void free_nodes(void)
-{
-stack_t *tmp;
-
-if (head == NULL)
-return;
-
-while (head != NULL)
-{
-tmp = head;
-head = head->next;
-free(tmp);
-}
-}
-
-/**
-* add_to_queue - Adds a node to the queue.
-* @new_node: Pointer to the new node.
-* @ln: line number of the opcode.
-*/
-void add_to_queue(stack_t **new_node, __attribute__((unused))unsigned int ln)
-{
-stack_t *tmp;
-
-if (new_node == NULL || *new_node == NULL)
-exit(EXIT_FAILURE);
-if (head == NULL)
-{
-head = *new_node;
-return;
-}
-tmp = head;
-while (tmp->next != NULL)
-tmp = tmp->next;
-
-tmp->next = *new_node;
-(*new_node)->prev = tmp;
 }
