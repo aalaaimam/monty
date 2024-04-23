b362833b306140c2436e03e56488810c6d2f8ccb
aalaaimam <aalaaimam.com>
2024-04-23 13:29:01 -0700

    monty

--- file_tools.c
+++ file_tools.c
@@ -1,28 +1,75 @@
 #include "monty.h"
 
-void open_file(char *file_name, stack_t **head)
+/**
+* open_file - Opens a file.
+* @file_name: Name of the file to open.
+* Return: File descriptor on success, -1 on failure.
+*/
+int open_file(char *file_name)
 {
-/* Implementation of open_file */
+int fd;
+
+fd = open(file_name, O_RDONLY);
+if (fd == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't open file %s\n", file_name);
+exit(EXIT_FAILURE);
+}
+return (fd);
 }
 
-void free_nodes(stac_t **head)
+/**
+* free_nodes - Frees nodes in the stack.
+* @head: Pointer to the head of the stack.
+*/
+void free_nodes(stack_t *head)
 {
-/* Implementation of free_nodes */
+stack_t *tmp;
+
+while (head != NULL)
+{
+tmp = head;
+head = head->next;
+free(tmp);
+}
 }
 
-stack_t *create_node(int data)
+/**
+* create_node - Creates a node.
+* @n: Number to go inside the node.
+* Return: Upon success, a pointer to the node. Otherwise, NULL.
+*/
+stack_t *create_node(int n)
 {
-stack_t *node = malloc(sizeof(stack_t));
+stack_t *node;
+
+node = malloc(sizeof(stack_t));
 if (node == NULL)
 {
-/* Handle error */
+dprintf(STDERR_FILENO, "Error: malloc failed\n");
+exit(EXIT_FAILURE);
 }
-node->data = data;
+node->n = n;
 node->next = NULL;
-return node;
+return (node);
 }
 
-void add_to_queue(stack_t **new_node, stack_t **head, unsigned int ln)
+/**
+* add_to_queue - Adds a node to the queue.
+* @new_node: Pointer to the new node.
+* @head: Pointer to the head of the stack.
+*/
+void add_to_queue(stack_t **new_node, stack_t **head)
 {
-/* Implementation of add_to_queue */
+stack_t *tmp;
+
+if (*head == NULL)
+{
+*head = *new_node;
+return;
+}
+tmp = *head;
+while (tmp->next != NULL)
+tmp = tmp->next;
+tmp->next = *new_node;
 }
